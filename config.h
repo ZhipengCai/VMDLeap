ebec6968d539112d67cb9ee993b251f64a4c99ef
Ryohei SUZUKI <quolc.i@gmail.com>
2013-11-22 16:28:12 +0900

    initial commit

---
+++ config.h
@@ -0,0 +1,25 @@
+#ifndef CONFIG_OPTIONS_H
+#define CONFIG_OPTIONS_H
+#define VMDVERSION "1.9.1"
+#define VERSION_MSG "VMD for MACOSXX86_64, version 1.9.1 (November 15, 2013)"
+/* You should not change this value */
+#define CMDLEN 15
+#define DEF_VMDIMAGEVIEWER "/usr/bin/open %s"
+#define DEF_VMDENVVAR "/Users/quolc/vmd/lib/vmd"
+#define INIT_DEFDISPLAY DISPLAY_WIN
+#define INIT_DEFTITLE TITLE_ON
+#define INIT_DEFHEIGHT 6.0
+#define INIT_DEFDIST -2.0
+#define VMD_AUTHORS "R. Brunner, E. Caddigan, J. Cohen, A. Dalke, P. Grayson, J. Gullingsrud, D. Hardy, W. Humphrey, B. Isralewitz, S. Izrailev, A. Kohlmeyer, D. Norris, J. Saam, J. Stone, J. Ulrich"
+#define VMD_AUTHORS_LINE1 "R. Brunner, E. Caddigan, J. Cohen, A. Dalke, P. Grayson, "
+#define VMD_AUTHORS_LINE2 "J. Gullingsrud, D. Hardy, W. Humphrey, B. Isralewitz, S. Izrailev, "
+#define VMD_AUTHORS_LINE3 "A. Kohlmeyer, D. Norris, J. Saam, J. Stone, J. Ulrich"
+#define VMD_AUTHORS_LINE4 ""
+#define VMD_STARTUP ".vmdrc"
+#define PYVMD_STARTUP ".pyvmdrc"
+#define VMD_ARCH "MACOSXX86_64"
+#define VMD_OPTIONS "MACOSXX86_64 FLTKOPENGL FLTK TK OPENCL IMD TCL PTHREADS SILENT LP64"
+#define VMD_HOMEPAGE "http://www.ks.uiuc.edu/Research/vmd/"
+#define VMD_HELPPAGE "http://www.ks.uiuc.edu/Research/vmd/"
+#define VMD_CMD_PROMPT "vmd > "
+#endif
